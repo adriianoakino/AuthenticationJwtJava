7d877dbeeed528f84a1bf797b26a3353cbc08289
Adriano De Aquino Oliveira Santos <aasantos9@stefanini.com>
2019-09-24 10:01:45 -0300

    feat: incrementado o Jasypt, e properties de dev

--- src/main/java/com/fatec/ProjetoSistemasInformacoesApplication.java
+++ src/main/java/com/fatec/ProjetoSistemasInformacoesApplication.java
@@ -3,7 +3,10 @@ package com.fatec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+
 @SpringBootApplication
+@EncryptablePropertySource(name = "EncryptedPropertiesDev", value = "classpath:application-dev.properties")
 public class ProjetoSistemasInformacoesApplication {
 
 	public static void main(String[] args) {
